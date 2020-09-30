59dabcd123fa2285d354319e46445d9aa2d73987
Brad Alexander <bradalexander@Brads-MacBook-Pro.local>
2020-09-30 15:51:45 -0500

    initialized app, made a blog page, and added blog posts using []syntax and json

---
+++ pages/blog/[id].js
@@ -0,0 +1,17 @@
+import { useRouter } from 'next/router'
+import posts from '../../posts.json'
+
+export default () => {
+    const router = useRouter()
+
+    const post =posts[router.query.id]
+    if (!post) return <p></p>    
+
+    return (
+        <>
+        <h1>{post.title}</h1>
+        <p>Post id: {router.query.id}</p>
+        <p>{post.content}</p>
+        </>
+    )
+}
